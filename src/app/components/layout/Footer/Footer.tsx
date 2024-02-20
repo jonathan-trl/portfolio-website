7ba8f0112d29f76b9645669ccc32fb227f6bea7a
Jonathan Carvalho <jonathan.carvalho1807@gmail.com>
2024-02-20 14:01:19 -0300

    footer corrections

--- src/app/components/layout/Footer/Footer.tsx
+++ src/app/components/layout/Footer/Footer.tsx
@@ -4,15 +4,30 @@ import { IconLink } from '../../common/IconLink'
 
 export const Footer = () => {
   return (
-    <footer className="bg-[#7B4AE21A] flex justify-center gap-4 py-4">
-      <IconLink
-        href={linkedinLink}
-        icon={<FaLinkedin size={23} color="rgba(255,255,255,.6)" />}
-      />
-      <IconLink
-        href={githubLink}
-        icon={<FaGithub size={23} color="rgba(255,255,255,.6)" />}
-      />
+    <footer className="relative bg-[#7B4AE21A] flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-4 py-4">
+      <p className="text-white/55 text-[12px] sm:text-base order-2 sm:order-1">
+        © 2024 · Desenvolvido por Jonathan Carvalho
+      </p>
+      <div className="order-1 sm:order-2 flex item-center gap-4">
+        <IconLink
+          href={linkedinLink}
+          icon={
+            <FaLinkedin
+              size={23}
+              className="text-white/50 hover:text-white transition-colors"
+            />
+          }
+        />
+        <IconLink
+          href={githubLink}
+          icon={
+            <FaGithub
+              size={23}
+              className="text-white/50 hover:text-white transition-colors"
+            />
+          }
+        />
+      </div>
     </footer>
   )
 }
